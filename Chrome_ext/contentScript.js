e60a0e78300af480527453570fcf5e94bde87cd7
LirGal <galantilir@gmail.com>
2022-12-16 16:10:46 +0200

    clear cart working & global dict updated

--- Chrome_ext/contentScript.js
+++ Chrome_ext/contentScript.js
@@ -1,3 +1,6 @@
+
+// maps: url->item->details
+// example: CART_DICT[https...website1..][red shirt] == {color:"red", size:"large"....}
 const CART_DICT = "CART_DICT";
 
 // add observer for the cart
@@ -16,41 +19,44 @@ function cart_change_handler(mutationRecList) {
     update_cart_dict(temp_dict, result.CART_DICT);
     console.log("CART_DICT");
     console.log(result.CART_DICT);
+    return true;
   });
   return true; // elimate server error
 }
 
 // deleting the cart when the user clears it
-// TODO - this does nothing so far, we need to work on deleting items from only one website.
-// TODO - currently "clear" clears global func, only works in "comfy house" website
 function clear_cart_handler(mutationRecList) {
   // background.CART_DICT = {}; -> cannot uset this because it clears all the carts
   console.log("CART CLEARED"); // TODO - delete this line afer imp.
-  chrome.storage.local.set({ CART_DICT: {} }, function(){}); // set global dict to empty
+  var update_CART_DICT = {};
+  update_CART_DICT[location.href] = {};
+  chrome.storage.local.get([CART_DICT]).then((result) => {
+    update_cart_dict(update_CART_DICT, result.CART_DICT);
+    return true;
+  });
   return true;
 }
 
 // re-read the cart json and update the global cart_dict
-// TODO - create a way to figure out which website the item is coming from
 function create_cart_dict() {
   
-  var temp_CART_DICT = {};
-  for (const item of Array.from(document.getElementsByClassName("cart-item"))) {
-
-    item_name   = item.querySelector("h4").innerHTML;
-    item_price  = item.querySelector("h5").innerHTML;
-    item_amount = item.querySelector("p").innerHTML;
-    temp_CART_DICT[item_name] = {price:item_price, amount:item_amount };
-
+  console.log(JSON.parse(localStorage.getItem("cart")));
+  console.log(typeof JSON.parse(localStorage.getItem("cart")));
+  curr_shop_dict = {};
+  for (const item_dict of JSON.parse(localStorage.getItem("cart"))) {
+    curr_shop_dict[item_dict["title"]] = item_dict;
   }
-  return temp_CART_DICT;
+
+  var update_CART_DICT = {};
+  update_CART_DICT[location.href] = curr_shop_dict;
+  return update_CART_DICT;
 }
 
 function update_cart_dict(temp_dict, global_dict) {
-
+  
   for (const [key, value] of Object.entries(temp_dict)) {
     global_dict[key] = value;
   }
-
+  
   chrome.storage.local.set({ CART_DICT: global_dict }, function(){});
 }
