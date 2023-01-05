4047e92a7e686c8ac6d7d5e4e0697b263c9b6c94
LirGal <galantilir@gmail.com>
2023-01-05 21:41:56 +0200

    small bug in syncing UI fixed

--- Chrome_ext/background.js
+++ Chrome_ext/background.js
@@ -1,5 +1,6 @@
 // this is the global dictionary that will hold all the values.
-var SUPERCART_TAB_ID;
+var SUPERCART_TAB_ID = -1;
+var TAB_OPENED = 
 chrome.action.onClicked.addListener(function () {
     chrome.tabs.create(
         { url: "MyCart/index.html" },
@@ -12,7 +13,8 @@ chrome.action.onClicked.addListener(function () {
 // refreshing the UI every time a cart is updated
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-      if (request.refresh_ui_tab === "true")
-        chrome.tabs.reload(SUPERCART_TAB_ID);
+      if (request.refresh_ui_tab === "true" && (SUPERCART_TAB_ID != -1) ){
+          chrome.tabs.reload(SUPERCART_TAB_ID);
+      }
     }
   );
